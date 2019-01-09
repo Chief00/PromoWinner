d05a3e979179cacad27736369ccfcab9af6745d8
Joshua <joshuajmolloy@gmail.com>
2019-01-09 20:16:11 +0000

    Added access keys

--- scrape.py
+++ scrape.py
@@ -1,3 +1,8 @@
 import tweepy
 
+API_Key = "ZOXLUeZrRxZA2Z9Q5b3UaNpbh"
+API_Secret_Key = "nEW3yCXzC0DSPp3fSEG74fkztB4jOsxtrXw9r07OSOWql1Ii4n"
+Access_Token = "308574125-vDu3m6myZRUBA6vmjJCmEE2n2hzQS9hGRLSpRtIj"
+Access_Token_Secret = "2bAoI8yIdbgVyIEQfGptSYJ6eNtKMRheE7rcx1a2RdxV9"
+
 API.user_timeline("sam_beckman",)
