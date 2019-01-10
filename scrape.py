025a8d4d504f15339cb7e8f4c407c74a1a244049
Joshua <joshuajmolloy@gmail.com>
2019-01-10 22:52:43 +0000

    Finished

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 import tweepy
-import re
 import requests
+import os
 
 # All the access keys to authenticate
 API_Key = "ZOXLUeZrRxZA2Z9Q5b3UaNpbh"
@@ -11,20 +11,49 @@ Access_Token_Secret = "2bAoI8yIdbgVyIEQfGptSYJ6eNtKMRheE7rcx1a2RdxV9"
 # Authenticating and creatign the api variable for use
 auth = tweepy.OAuthHandler(API_Key, API_Secret_Key)
 auth.set_access_token(Access_Token, Access_Token_Secret)
-API = tweepy.API(auth)
+API = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
 def userTweets(user, ntweets):
     # Getting last tweet from user
-    userTweet = API.user_timeline(user, count=ntweets)[0].text
+    userTweet = API.user_timeline(user, count=ntweets)[0]
     return userTweet
 
 
-tweetString = userTweets("sam_beckman", 1)
+# Inputs tweet into variable creates variable oldTweetID
 
-# Regex tweet link
-tweetLinkRE = re.compile('https://t.co/\w+')
+oldTweetID = '1083198705275002880'
+OCRLooper = True
+mailLoop = True
 
+while mailLoop:
+    # Gets tweet from twitter
+    tweet = userTweets("sam_beckman", 1)
 
-if "promo" and "KLCK" in tweetString:
-    print(tweetLinkRE.search(tweetString).group())
+    # Checks to see if its a new tweet
+    if tweet["id"] != oldTweetID:
+        oldTweetID = tweet["id"]
+
+        # Creates variable of the text in tweet
+        tweetText = tweet["text"]
+
+        # Checks if text is about KLCK promo code and downloads the image
+        if "promo" and "KLCK" in tweetText:
+            tweetImageURL = tweetText["entities"]["media"][0]["media_url"]
+            twitterImage = requests.get(tweetImageURL)
+            open("PromoCodes.jpg", 'wb').write(twitterImage.content)
+
+            # Runs tesseract
+            os.system("tesseract PromoCodes.jpg PromoCodesText")
+
+            # Waits for the text file to be created
+            while OCRLooper:
+                if os.path.isfile("Promocodes.txt"):
+                    PromoCodesText = open(
+                        "PromoCodesText.txt", 'r').read().split()
+                    # loops through items in list and send a notification
+                    for i in range(2, len(PromoCodesText) - 1):
+                        os.system(
+                            "curl -u o.HKHTPzhnlx9AsQuDtOicBXAlqaZAkseV:poppy12 https://api.pushbullet.com/v2/pushes -d type=note -d title=\"Promo Codes\" -d body=" + str(PromoCodesText[i]))
+                OCRLooper = False
+                mailLoop = False
